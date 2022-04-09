fcf6facd558acc7fac57c41274da5cdde4e5ab79
Leonardo Behnck <leonardo.behnck@gmail.com>
2022-04-09 16:10:38 -0300

    commit para main

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Cadastro simples de usuário e envio de mensagens
+
+## 📁 Acesso ao projeto
+
+<a href="https://refined-github-html-preview.kidonng.workers.dev/leonardobehnck/usuario/raw/main/index.html">Clique aqui para acessar a demo do projeto.</a>
+
+## :hammer: Funcionalidades do projeto
+
+- `Funcionalidade 1`: Cadastro usuário com nome e cor respectiva;
+- `Funcionalidade 2`: Mostra na tela o nome do usuário cadastrado;
+- `Funcionalidade 3`: Seleciona o usuário cadastrado;
+- `Funcionalidade 4`: Envia uma mensagem do usuário com a cor;
+- `Funcionalidade 5`: Mostra na tela a mensagem enviada;
+
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
---
+++ script.js
@@ -0,0 +1,45 @@
+let usuarios = []
+
+function criarUsuario() {
+  let usuarioNome = document.getElementById('usuarioNome').value
+  let usuarioCor = document.getElementById('usuarioCor').value
+  let u = { usuarioNome: usuarioNome, usuarioCor: usuarioCor }
+
+  usuarios.push(u)
+  apagar()
+  exibeUsuarios()
+}
+
+function exibeUsuarios() {
+  document.getElementById('usuariosAtivos').innerHTML = ""
+  for (let i = 0; i < usuarios.length; i++) {
+    document.getElementById('usuariosAtivos').innerHTML += `<span id="usuarioCor" style="color:${usuarios[i].usuarioCor};">${usuarios[i].usuarioNome}</span>`
+  }
+  listaUsuarios()
+}
+
+function listaUsuarios() {
+  document.getElementById('listaUsuarios').innerHTML = ""
+  for (let i = 0; i < usuarios.length; i++) {
+    document.getElementById('listaUsuarios').innerHTML += `<option style="color:${usuarios[i].usuarioCor};">${usuarios[i].usuarioNome}</option>`
+  }
+}
+
+function exibeMensagemNaTela() {
+  let nomeDoUsuarioAtivo = document.getElementById('listaUsuarios').value
+  let mensagemUsuario = document.getElementById('mensagemUsuario').value
+  for (let i = 0; i < usuarios.length; i++) {
+    if (nomeDoUsuarioAtivo == usuarios[i].usuarioNome) {
+      let usuarioNome = usuarios[i].usuarioNome
+      let usuarioCor = usuarios[i].usuarioCor
+
+      document.getElementById('mensagemNaTela').innerHTML = `
+      <h2>Mensagem recebida!</h2><p id="mensagemEnviada" style="color:${usuarioCor};">${mensagemUsuario}</p><p id="enviadoPor">Enviado por: <strong>${usuarioNome}</strong></p>`
+    }
+  }
+}
+
+function apagar() {
+  document.getElementById('usuarioNome').value = ""
+  document.getElementById('usuarioCor').value = ""
+}
